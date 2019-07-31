c286aa4159c9cdbcf17866977468cb280ad05718
NahuelBarrios <52472582+NahuelBarrios@users.noreply.github.com>
2019-07-31 15:38:46 -0300

    EL PIBE CANTINA

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,7 +8,159 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+
+    //Todas las lamparas estan $35
+    var cantlamp;
+    var marca;
+    var desc;
+    var precio = 35;
+    var preciototal;
+    var preciofinal;
+
+    var impuesto;
+
+
+
+    cantlamp = parseInt(document.getElementById("Cantidad").value);
+
+    marca = document.getElementById("Marca").value;
+
+    desc = parseInt(document.getElementById("precioDescuento").value);
+
+   
+
+
+
+    //A
+
+    if (cantlamp >= 6) {
+
+        preciototal = cantlamp * precio;
+
+        desc = preciototal * 0.5;
+
+        preciofinal = preciototal - desc;
+
+    }
+
+    //B
+    else if (cantlamp == 5) {
+
+        switch ("Marca") {
+
+
+            case "Argentinaluz":
+
+                preciototal = cantlamp * precio;
+
+                desc = preciototal * 0.4;
+
+                preciofinal = preciototal - desc;
+
+                break;
+
+            default: preciototal = cantlamp * precio;
+
+                desc = preciototal * 0.3;
+
+                preciofinal = preciototal - desc;
+
+
+        }
+
+
+
+    }
+
+    //C
+
+    else if (cantlamp == 4) {
+
+        switch ("Marca") {
+
+            case "ArgentinaLuz":
+            case "FelipeLamparas":
+
+                preciototal = cantlamp * precio;
+
+                desc = preciototal * .25;
+
+                preciofinal = preciototal - desc;
+
+                break;
+
+            default: preciototal = cantlamp * precio;
+
+                desc = preciototal * .20;
+
+                preciofinal = preciototal - desc;
+
+
+        }
+
+
+
+    }
+
+
+    //D
+
+    else if (cantlamp == 3) {
+
+        switch ("Marca") {
+
+            case "ArgentinaLuz":
+                preciototal = cantlamp * precio;
+
+                desc = preciototal * .15;
+
+                preciofinal = preciototal - desc;
+
+                break;
+
+
+            case "FelipeLamparas":
+
+                preciototal = cantlamp * precio;
+
+                desc = preciototal * .10;
+
+                preciofinal = preciototal - desc;
+
+                break;
+
+            default: preciototal = cantlamp * precio;
+
+                desc = preciototal * .5;
+
+                preciofinal = preciototal - desc;
+        }
+
+    }
+
+
+
+
+
+
+    if(preciofinal>120){
+
+        impuesto= preciofinal * .10;
+
+        preciofinal= preciofinal + impuesto;
+
+        alert("IIBB Usted pago: "+ impuesto );
+
+    }
+
+    
+
+
+
+    
+
+
+
 }
+
